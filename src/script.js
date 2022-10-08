2ed7e813e934aa927ae5e66b20c91315ce139e81
Noam Rivlin <noam.rivlin15@gmail.com>
2022-10-08 16:15:41 +0300

    many changes

--- src/script.js
+++ src/script.js
@@ -7,26 +7,42 @@ const employees = gids[0];
 const hireDates = gids[1];
 const salaries = gids[2];
 // END OF DATA SETUP
-// below is the employee address taken from the browser for comparison
+// below is the employee address takens from the browser for comparison
 // 'https://docs.google.com/spreadsheets/d/1C1-em4w0yHmd2N7__9cCSFzxBEf_8r74hQJBsR6qWnE0'
 // TODO your code here
-const getEmployeeDate = async () => {
-  console.log(dataURLBase + id + dataURLEnd + employees);
-  let response = await fetch(dataURLBase + id + dataURLEnd + hireDates);
-  let data = await response.text();
-
-  console.log(
-    JSON.parse(
-      data
-        .toString()
-        .replace(
-          `/*O_o*/
-google.visualization.Query.setResponse(`,
-          ''
-        )
-        .replace(';', '')
-        .slice(0, -1)
-    )
+const fetchGoogleDatas = async () => {
+  const responses = await Promise.all(
+    gids.map(async (gid, index) => {
+      let response = await fetch(dataURLBase + id + dataURLEnd + gid);
+      let data = await response.text();
+      // JSON.parse(data.match(/(?<=.*\().*(?=\))/))
+      const FormData = JSON.parse(data.slice(47, -2));
+      const rows = FormData.table.rows;
+      if (rows.length === 11) {
+        rows.shift();
+      }
+      return rows;
+    })
   );
+  return responses;
 };
-getEmployeeDate();
+
+const createEmployees = async () => {
+  const responses = await fetchGoogleDatas();
+
+  let employees = [];
+  for (let i = 0; i < 10; i++) {
+    employees.push({
+      lastN: responses[0][i].c[1].v,
+      firstN: responses[0][i].c[0].v,
+      hireDate: new Date(responses[1][i].c[0].f).toDateString().slice(4),
+      salarie: Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(responses[2][i].c[0].v),
+    });
+  }
+  console.log(employees);
+  return employees;
+};
+createEmployees();
